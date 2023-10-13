1afd4ac66290e9580e403b3d354bab857f1c8b4f
Emma <emma.polanco@enti.cat>
2023-10-13 23:47:02 +0200

    Completed Exercice 1
    Added Exercice 1, Pragma regions to organize the code better, commented and added the readme.txt too.

--- AnimationFoundations_D1_emptyDLL/RobotController/MyRobotController.cs
+++ AnimationFoundations_D1_emptyDLL/RobotController/MyRobotController.cs
@@ -28,22 +28,60 @@ namespace RobotController
             return s;
         }
 
-        ////////////////////////////EX1: this function will place the robot in the initial position
+        #region Fields
+        // First, we need arrays to store initial and final joint angles
+        private float[] initialAngles;
+        // We need an array to store rotation axes for joints, that instead of being a float will need to be MyVec
+        private MyVec[] axisRotation;
+        // Variables for controlling iterations, that will simply add as flags
+        private bool iteration2ControlFlag;
+        private bool iteration3ControlFlag;
+        #endregion
+
+        #region Constructor
+        public MyRobotController()
+        {
+            // Initialize initialAngles
+            initialAngles = new float[5] { 74, -10, 80, 40, 0 };
+
+            // Initialize axisRotation
+            axisRotation = new MyVec[5];
+            axisRotation[0] = new MyVec { x = 0, y = 1, z = 0 };
+            axisRotation[1] = new MyVec { x = 1, y = 0, z = 0 };
+            axisRotation[2] = axisRotation[3] = axisRotation[1];
+            axisRotation[4] = new MyVec { x = 0, y = 1, z = 0 };
+
+            // Initialize control flags
+            iteration2ControlFlag = true;
+            iteration3ControlFlag = true;
+        }
+        #endregion
+
+
+
+        // EX1: this function will place the robot in the initial position
+        #region EX1
         public void PutRobotStraight(out MyQuat rot0, out MyQuat rot1, out MyQuat rot2, out MyQuat rot3) {
 
-            //todo: change this, use the function Rotate declared below
+            // Initialize rot0 with the identity quaternion
             rot0 = NullQ;
-            rot1 = NullQ;
-            rot2 = NullQ;
-            rot3 = NullQ;
-        }
 
+            // Calculate the rotations for each joint using the initial angles and rotation axes
+            rot0 = Rotate(rot0, axisRotation[0], (float)Radians(initialAngles[0]));
+            rot1 = Rotate(rot0, axisRotation[1], (float)Radians(initialAngles[1]));
+            rot2 = Rotate(rot1, axisRotation[2], (float)Radians(initialAngles[2]));
+            rot3 = Rotate(rot2, axisRotation[3], (float)Radians(initialAngles[3]));
+
+            // Reset control flags for iterations 2 and 3
+            iteration2ControlFlag = true;
+            iteration3ControlFlag = true;
+        }
+        #endregion
 
 
         //EX2: this function will interpolate the rotations necessary to move the arm of the robot until its end effector collides with the target (called Stud_target)
         //it will return true until it has reached its destination. The main project is set up in such a way that when the function returns false, the object will be droped and fall following gravity.
-
-
+        #region EX2
         public bool PickStudAnim(out MyQuat rot0, out MyQuat rot1, out MyQuat rot2, out MyQuat rot3)
         {
 
@@ -72,12 +110,12 @@ namespace RobotController
 
             return false;
         }
-
+        #endregion
 
         //EX3: this function will calculate the rotations necessary to move the arm of the robot until its end effector collides with the target (called Stud_target)
         //it will return true until it has reached its destination. The main project is set up in such a way that when the function returns false, the object will be droped and fall following gravity.
         //the only difference wtih exercise 2 is that rot3 has a swing and a twist, where the swing will apply to joint3 and the twist to joint4
-
+        #region EX3
         public bool PickStudAnimVertical(out MyQuat rot0, out MyQuat rot1, out MyQuat rot2, out MyQuat rot3)
         {
 
@@ -117,12 +155,9 @@ namespace RobotController
             return NullQ;
 
         }
-
-
-
-
         #endregion
 
+        #endregion
 
         #region private and internal methods
 
@@ -171,7 +206,7 @@ namespace RobotController
             float sinHalfAngle = (float)Math.Sin(halfAngle);
 
             //2  Create a new quaternion for rotation
-            MyQuat rotation;
+            MyQuat rotation = NullQ;
             rotation.w = (float)Math.Cos(halfAngle);
             rotation.x = axis.x * sinHalfAngle;
             rotation.y = axis.y * sinHalfAngle;
@@ -181,6 +216,11 @@ namespace RobotController
             return Multiply(currentRotation, rotation);
         }
         //todo: add here all the functions needed
+
+        internal double Radians(double angleRadians)
+        {
+            return (angleRadians * (Math.PI / 180));
+        }
         #endregion
     }
 }
